28abeff115a46a110cfa9df3decdcb3db2bf1022
sanketb412 <sanketbagde38@gmail.com>
2021-05-18 11:18:51 +0530

    Added: code of Welcomming Message

---
+++ main/java/com/address/AddressBook.java
@@ -0,0 +1,8 @@
+package com.address;
+
+public class AddressBook {
+
+    public static void main (String[] args) {
+        System.out.println("Welcome to Address Book Program in AddressBook in Main Class");
+    }
+}
