0c96b51dd57b79cdc73e36f0dd8991b7137e2510
sanketb412 <sanketbagde38@gmail.com>
2021-05-18 11:50:43 +0530

    Added: code for Entering contacts in AddressBook

--- main/java/com/address/AddressBook.java
+++ main/java/com/address/AddressBook.java
@@ -1,5 +1,7 @@
 package com.address;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
     /** Address Book inputs**/
@@ -8,8 +10,8 @@ public class AddressBook {
     int zip;
 
     /** Defining each elements **/
-    public AddressBook (String firstName,String lastName,String address,String city,String state,
-                        int zip,String eMail, String phoneNumber) {
+    public  AddressBook (String firstName,String lastName,String address,String city,String state,
+                         int zip,String eMail, String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -19,6 +21,35 @@ public class AddressBook {
         this.eMail = eMail;
         this.phoneNumber = phoneNumber;
     }
+
+    public AddressBook() {
+
+    }
+    /** Getting input from User **/
+    private void addContactDetails() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First Name");
+        String First_Name = sc.nextLine();
+        System.out.println("Enter the Last Name");
+        String Last_Name = sc.nextLine();
+        System.out.println("Enter the Address");
+        String Address = sc.nextLine();
+        System.out.println("Enter the City");
+        String City = sc.nextLine();
+        System.out.println("Enter the State");
+        String State = sc.nextLine();
+        System.out.println("Enter the Zip Code");
+        int zip = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the Contact Number");
+        String Contact_Number = sc.nextLine();
+        System.out.println("Enter the Email address");
+        String eMail = sc.nextLine();
+        /** Calling addressBook **/
+        AddressBook entry = new AddressBook(First_Name, Last_Name, Address, City, State, zip, Contact_Number, eMail);
+        entry.addressBook();
+    }
+
     /** Printing the Elements **/
     public void addressBook() {
         System.out.println("First Name: " + firstName);
@@ -33,7 +64,7 @@ public class AddressBook {
 
     public static void main (String[] args) {
         System.out.println("Welcome to Address Book Program in AddressBook in Main Class");
-        AddressBook book = new AddressBook(null,null,null,null,null,0,null,null);
-        book.addressBook();
+        AddressBook book = new AddressBook();
+        book.addContactDetails();  /** Calling addContactDetails **/
     }
 }
